39bb482dd5e56e7ef90ce9a890208214169f575f
MrHencke <hencke1@hotmail.com>
2021-02-10 12:19:29 +0100

    Removed unused constants

--- src/no/hvl/dat109/constants/Constants.java
+++ src/no/hvl/dat109/constants/Constants.java
@@ -1,27 +1,11 @@
 package no.hvl.dat109.constants;
 
 public class Constants {
-    
+
     public static final String DATE_TIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
 
     public static final String DATE_FORMAT = "dd-MM-yyyy";
 
     public static final String TIME_FORMAT = "HH:mm:ss";
 
-    /*
-    public static final int PRICE_GROUP_A = 0;
-
-    public static final int PRICE_GROUP_B = 0;
-
-    public static final int PRICE_GROUP_C = 0;
-
-    public static final int PRICE_GROUP_D = 0;
-
-    public static int getPrice(char c) {
-        return c == 'A' ? PRICE_GROUP_A 
-            : c == 'B' ? PRICE_GROUP_B 
-            : c == 'C' ? PRICE_GROUP_C
-            : PRICE_GROUP_D;
-    }
-    */
 }
