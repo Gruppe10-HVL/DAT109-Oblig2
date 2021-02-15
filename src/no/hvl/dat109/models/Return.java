ed3e08dab8f6b6347babf57ab17e3a8c33fc7acc
MrHencke <hencke1@hotmail.com>
2021-02-15 14:06:54 +0100

    return class

--- src/no/hvl/dat109/models/Return.java
+++ src/no/hvl/dat109/models/Return.java
@@ -1,10 +1,54 @@
 package no.hvl.dat109.models;
 
+import java.time.LocalDateTime;
+
+import no.hvl.dat109.utils.CreditCard;
+
 public class Return {
+    private CreditCard creditCard;
+    private LocalDateTime currentDate;
+    private String regnr;
+    private int mileage;
 
-    // CreditCard, LocalDateTime currentDate, Regnr, mileage
-    public Return() {
-
+    public Return(CreditCard creditCard, LocalDateTime currentDate, String regnr, int mileage) {
+        this.creditCard = creditCard;
+        this.currentDate = currentDate;
+        this.regnr = regnr;
+        this.mileage = mileage;
     }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public LocalDateTime getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(LocalDateTime currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getRegnr() {
+        return regnr;
+    }
+
+    public void setRegnr(String regnr) {
+        this.regnr = regnr;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
     
+
 }
