b2286a2b0a76e817242bc840d10b9c5c8a710e6a
MrHencke <hencke1@hotmail.com>
2021-02-15 11:11:50 +0100

    Added main method

---
+++ src/no/hvl/dat109/Main.java
@@ -0,0 +1,19 @@
+package no.hvl.dat109;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+
+}
